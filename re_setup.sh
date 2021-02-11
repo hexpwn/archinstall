7f99ba9776415aa35a8646665a4d7a68e0c2e49d
none <none>
2021-02-11 16:24:55 +0000

    First commit

---
+++ re_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo pacman -S ghidra gdb ltrace strace checksec gnu-netcat nmap dnsutils \
+	mitmproxy binwalk
+
+mkdir -p ~/lab/apps
+cd ~/lab/apps
+
+git clone https://github.com/radareorg/radare2.git
+cd radare2
+./sys/install.sh
+
+
+# Instal gdb-fef
+cd ~
+wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
+echo source ~/.gdbinit-gef.py >> ~/.gdbinit
---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server lightdm lightdm-gtk-greeter i3-wm i3status rxvt-unicode urxvt-perls dmenu vim htop bluez bluez-utils pavucontrol tmux python pamixer feh man-pages wget ufw xclip unzip tree torsocks net-tools iw
